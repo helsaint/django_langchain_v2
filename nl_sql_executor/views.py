f9225326af43e2081dc724c807841725f154a391
helsaint <alexei.ramotar@gmail.com>
2025-08-19 20:57:50 -0400

    Minor formatting changes.

--- nl_sql_executor/views.py
+++ nl_sql_executor/views.py
@@ -3,11 +3,11 @@ from django.http import JsonResponse
 from .utils.nl_sql import sql_chain
 
 def nl_sql_executor(request):
-
     if request.method == "POST":
         nl = request.POST.get("nl", "")
         nl = nl.lower()
         result = sql_chain.invoke({"question": nl})
-        return JsonResponse({"message": [result['answer'],]})
+        print(result)
+        return JsonResponse({"message": [result['answer']]})
     return render(request, "nl_executor.html")
 # Create your views here.
